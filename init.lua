8fbed42e91bd95a53c450bdfb9620b0a35ca5fe8
kitaryot <kita.ryosuke.56w@st.kyoto-u.ac.jp>
2024-07-27 09:34:12 +0900

    10jを使用可能に

--- init.lua
+++ init.lua
@@ -954,11 +954,17 @@ if vim.g.vscode then
     end
   end
 
-  vim.keymap.set('n', 'k', moveCursor('up'), { expr = true })
-  vim.keymap.set('n', 'j', moveCursor('down'), { expr = true })
+  vim.keymap.set('n', 'k', moveCursor('up'), { expr = false })
+  vim.keymap.set('n', 'j', moveCursor('down'), { expr = false })
+  vim.keymap.set('n', '0', moveCursor('wrappedLineStart'), { expr = true })
+  vim.keymap.set('n', '^', moveCursor('wrappedLineFirstNonWhitespaceCharacter'), { expr = true })
+  vim.keymap.set('n', '$', moveCursor('wrappedLineEnd'), { expr = true })
 
-  vim.keymap.set('v', 'k', moveCursor('up', true), { expr = true })
-  vim.keymap.set('v', 'j', moveCursor('down', true), { expr = true })
+  vim.keymap.set('v', 'k', moveCursor('up', true), { expr = false })
+  vim.keymap.set('v', 'j', moveCursor('down', true), { expr = false })
+  vim.keymap.set('v', '0', moveCursor('wrappedLineStart', true), { expr = true })
+  vim.keymap.set('v', '^', moveCursor('wrappedLineFirstNonWhitespaceCharacter', true), { expr = true })
+  vim.keymap.set('v', '$', moveCursor('wrappedLineEnd', true), { expr = true })
 end
 
 vim.notify('initialization complated')
